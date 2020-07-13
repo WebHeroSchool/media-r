30e90b1f937db041d4266f979750c41d5a6b83ef
Regina <saifieva98@gmail.com>
2020-07-13 22:20:56 +0500

    add file js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+ var  menu = document.getElementById("nav-mobule");
+ var nav =  document.getElementById("nav-link");
+menu.addEventListener('click', function(){
+  nav.style.visibility = "visible";
+});
+}
